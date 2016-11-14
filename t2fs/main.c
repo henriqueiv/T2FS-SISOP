28aa5c5fa4d9de607182e07f6255819d2dbcd621
Henrique Valcanaia <henriqueindalencio@gmail.com>
2016-11-14 18:31:37 -0200

    Initial Commit

---
+++ t2fs/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  t2fs
+//
+//  Created by Henrique Valcanaia on 14/11/16.
+//  Copyright © 2016 SISOP. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
